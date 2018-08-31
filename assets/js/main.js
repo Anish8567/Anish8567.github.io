897bd63c980725a2d1395e102e7206819a6dde82
Anish Gadekar <anishgoa1@gmail.com>
2018-08-31 14:08:27 +0530

    pre load image efect

---
+++ assets/js/main.js
@@ -0,0 +1,185 @@
+$(document).ready(function(){
+	resizeDiv();
+	skrollrInit();
+});
+
+window.onresize = function(event) {
+	resizeDiv();
+	skrollrInit();
+}
+
+function resizeDiv() {
+	vpw = $(window).width();
+	vph = $(window).height();
+	navbar = $('.nav-outer-container').height();
+	videoheight= vph - navbar;
+	$('#myVideo').css({'height': videoheight + 'px'});
+	$('#tickets-outer-container').css({'min-height': videoheight + 'px'});
+}
+
+$(document).ready(function(){
+	$( ".video-cover-img"  ).last().addClass( "right-border-radius" );
+	$( ".video-cover-img"  ).first().addClass( "left-border-radius" );
+});
+
+$(document).ready(function(){
+	$.ajax("https://docs.google.com/spreadsheets/d/e/2PACX-1vQy7PRXMh6GB88jf-4uDGdSfyx8qwEmQJQ6IxvSc9tQHyBGd3VpVNi8OW6GuHizI8TdVj-otfdeHCPw/pub?gid=0&single=true&output=csv").done(function(result){
+	    csvJSON(result);
+	});
+});
+
+function csvJSON(csv){
+  // console.log("inside csv to json function");
+  var lines=csv.split("\n");
+  var result = [];
+  var headers=lines[0].split(",");
+  for(var i=1;i<lines.length;i++){
+	  var obj = {};
+	  var currentline=lines[i].split(",");
+	  for(var j=0;j<headers.length;j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+	  result.push(obj);
+  }
+  var data = JSON.stringify(result); //JSON
+  test(result);
+}
+
+function test(data){
+	for (var i = data.length - 1; i >= 0; i--) {
+		var template = $('#tickets_template').html();
+		if (template) {
+			var html = Mustache.to_html(template, data[i]);
+			$('.tickets-container').append(html);
+		}
+	}
+}
+
+$(document).ready(function(){
+	$('.team-dropdown').hover(function() {
+	  $(this).find('.team-dropdown-content').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+	  $(this).find('.team-dropdown-content').stop(true, true).delay(200).fadeOut(500);
+	});
+
+});
+
+//scroll to animation
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	},1000 );
+});
+
+$(document).ready(function() {
+	var formUrl= "http://128.199.218.232:89/eic-contact/";
+      $('form').submit(function(evt){
+      	console.log("inside form");
+      	var $this = $(this);
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var contact = $('#phone').val();
+		var description = $('#description').val();
+		var formdata = $this.serialize();
+		console.log(formdata);
+		console.log(name);
+		if ($this.attr('id') == 'contact-us') {
+			console.log("sending...");
+			$('#contact-us .mail-button ').text("Sending...");
+		}
+		if (!name == '' && !email == '' && !contact == '' && !description == '') {
+			console.log("1");
+			$.ajax({
+				type: 'POST',
+				data: formdata,
+				url: formUrl,
+				success: function(data) {
+					console.log("mail sent");
+					clearForm();
+					 // location.reload();
+                },
+				error: function(data) {
+					console.log(data);
+					alert('Sorry, something went wrong! Please try again ');
+				}
+			});
+		}
+
+        evt.preventDefault();
+      });
+ });
+function clearForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("description").value = "";
+    $('#contact-us .mail-button ').text("Send Mail");
+}
+var t;
+//function Skrollr
+function skrollrInit() {
+
+    //initialize skrollr
+	if($(window).width() > 600)
+	{
+
+		 skrollr.init();
+		 t= 1;
+	    // disable skrollr if using handheld device
+	    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	        skrollr.init().destroy();
+	    }
+	    else{}
+	}
+	else {
+
+		if( t == 1){
+ 				
+			if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+		        skrollr.init().destroy();
+	    	}
+
+			t=0;
+			}
+
+		else{} 
+
+	}
+
+}
+//blur effects
+
+window.addEventListener('load', function() {
+	
+	// setTimeout to simulate the delay from a real page load
+	setTimeout(lazyLoad, 1000);
+	
+});
+
+function lazyLoad() {
+	var card_images = document.querySelectorAll('.main-img');
+	
+	// loop over each card image
+	card_images.forEach(function(card_image) {
+		var image_url = card_image.getAttribute('data-image-full');
+		var content_image = card_image.querySelector('img');
+		
+		// change the src of the content image to load the new high res photo
+		content_image.src = image_url;
+		
+		// listen for load event when the new photo is finished loading
+		// content_image.addEventListener('load', function() {
+			// swap out the visible background image with the new fully downloaded photo
+			// card_image.style.backgroundImage = 'url(' + image_url + ')';
+			// add a class to remove the blur filter to smoothly transition the image change
+			// card_image.className = card_image.className + ' is-loaded';
+		// });
+		
+	});
+	
+}
