d34d5bd4f915c05bcee138728600e2f042b0d300
Anish Gadekar <anishgoa1@gmail.com>
2018-08-31 19:24:21 +0530

    cta for booking

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,30 +174,16 @@ function lazyLoad() {
 			content_image.src = image_url;
 
 			content_image.addEventListener('load', function() {
-			// swap out the visible background image with the new fully downloaded photo
-			// card_image.style.backgroundImage = 'url(' + image_url + ')';
-			// add a class to remove the blur filter to smoothly transition the image change
 			card_image.className = card_image.className + ' is-loaded';
 		});
 		}
-		else{
-			console.log("no");
-		}
+		else{}
 
-		// var image_url = card_image.getAttribute('data-image-full');
-		// var content_image = card_image.querySelector('img');
-		
-		// change the src of the content image to load the new high res photo
-		// content_image.src = image_url;
-		
-		// listen for load event when the new photo is finished loading
-		// content_image.addEventListener('load', function() {
-			// swap out the visible background image with the new fully downloaded photo
-			// card_image.style.backgroundImage = 'url(' + image_url + ')';
-			// add a class to remove the blur filter to smoothly transition the image change
-			// card_image.className = card_image.className + ' is-loaded';
-		// });
-		
 	});
 	
+}
+
+function addDescription(data){
+	console.log(data);
+	document.getElementById("description").value = "Book " +data+" for a gig";
 }
